759dbe782a4cd781b3fd595c1d3c53c87ce5c83a
Joshua <youjoshua12@gmail.com>
2018-11-10 21:48:55 -0800

    replace test code with quickstart

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motionplanningtest/opmode/TrackWidthCalibrationOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motionplanningtest/opmode/TrackWidthCalibrationOpMode.java
@@ -1,26 +1,36 @@
-package org.firstinspires.ftc.teamcode.motionplanningtest;
+package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.acmerobotics.roadrunner.drive.MecanumDrive;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.internal.system.Misc;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDriveBase;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDriveREV;
+
 /*
- * This routine measures the effective track width of the drivetrain (i.e., the distance between a
+ * This routine measures the effective track width of the drive (i.e., the distance between a
  * pair of wheels on opposite sides of the robot). This is required for the robot turn properly
  * during open-loop control.
+ *
+ * Note: this routine is *deprecated*; NewTrackWidthCalibrationOpMode is recommended instead.
  */
 @Config
+@Deprecated
+@Disabled
 @Autonomous
-public abstract class TrackWidthCalibrationOpMode extends LinearOpMode {
-    public static int TOTAL_REVOLUTIONS = 4;
+public class TrackWidthCalibrationOpMode extends LinearOpMode {
+    public static int TOTAL_REVOLUTIONS = 10;
     public static double POWER = 0.3;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        BNO055IMU imu = drive.getIMU();
+        SampleMecanumDriveBase drive = new SampleMecanumDriveREV(hardwareMap);
+        // it's important that the IMU/gyro/heading sensor is not part of the localization
+        drive.setLocalizer(new MecanumDrive.MecanumLocalizer(drive, false));
 
         telemetry.log().add("Press play to begin the track width calibration routine");
         telemetry.log().add("Make sure your robot has enough clearance to turn smoothly");
@@ -28,6 +38,8 @@ public abstract class TrackWidthCalibrationOpMode extends LinearOpMode {
         telemetry.update();
 
         waitForStart();
+
+        if (isStopRequested()) return;
 
         telemetry.log().clear();
         telemetry.log().add("Running...");
@@ -39,11 +51,8 @@ public abstract class TrackWidthCalibrationOpMode extends LinearOpMode {
 
         drive.setPoseEstimate(new Pose2d());
         drive.setVelocity(new Pose2d(0.0, 0.0,  POWER));
-        while (opModeIsActive() && (!startedMoving || revolutions <= TOTAL_REVOLUTIONS)) {
-            double heading = imu.getAngularOrientation().firstAngle;
-            if (imu.getParameters().angleUnit == BNO055IMU.AngleUnit.DEGREES) {
-                heading = Math.toRadians(heading);
-            }
+        while (!isStopRequested() && (!startedMoving || revolutions <= TOTAL_REVOLUTIONS)) {
+            double heading = drive.getExternalHeading();
             if (heading >= Math.PI / 2.0) {
                 startedMoving = true;
             }
@@ -54,14 +63,14 @@ public abstract class TrackWidthCalibrationOpMode extends LinearOpMode {
             lastHeading = heading;
         }
         drive.setVelocity(new Pose2d(0.0, 0.0, 0.0));
-        double effectiveTrackWidth = drive.getPoseEstimate().getHeading() / (4.0 * Math.PI * TOTAL_REVOLUTIONS);
+        double effectiveTrackWidth = drive.getPoseEstimate().getHeading() / (2.0 * Math.PI * TOTAL_REVOLUTIONS);
 
         telemetry.log().clear();
         telemetry.log().add("Calibration complete");
-        telemetry.log().add(String.format("effective track width = %.2f", effectiveTrackWidth));
+        telemetry.log().add(Misc.formatInvariant("Effective track width = %.2f", effectiveTrackWidth));
         telemetry.update();
 
-        while (opModeIsActive()) {
+        while (!isStopRequested()) {
             idle();
         }
     }
