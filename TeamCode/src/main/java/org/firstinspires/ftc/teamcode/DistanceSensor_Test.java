7c14a0eb1bb070bf5324fc33cc480c83744de520
Andrew Zhang <Azhang1818@gmail.com>
2018-11-25 10:17:47 -0800

    changed code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensor_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensor_Test.java
@@ -17,10 +17,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
  */
 
 public class DistanceSensor_Test extends LinearOpMode {
-    DistanceSensor sensorRange1;
-    DistanceSensor sensorRange2;
-    DistanceSensor sensorRange3;
-    DistanceSensor sensorRange4;
+    private DistanceSensor sensorRange1;
+    private DistanceSensor sensorRange2;
+    private DistanceSensor sensorRange3;
+    private DistanceSensor sensorRange4;
     double distanceS1;
     double distanceS2;
     double distanceS3;
@@ -38,34 +38,53 @@ public class DistanceSensor_Test extends LinearOpMode {
     Rev2mDistanceSensor sensorTimeOfFlight4 = (Rev2mDistanceSensor) sensorRange4;
 
 
+
+    ;
+
     @Override
     public void runOpMode() {
-
-
+        // you can use this as a regular DistanceSensor.
         sensorRange1 = hardwareMap.get(DistanceSensor.class, "Ds1");
         sensorRange2 = hardwareMap.get(DistanceSensor.class, "Ds2");
         sensorRange3 = hardwareMap.get(DistanceSensor.class, "Ds3");
-        //sensorRange4 = hardwareMap.get(DistanceSensor.class, "Ds4");
-        distanceS1 = sensorRange1.getDistance(DistanceUnit.CM);
-        distanceS2 = sensorRange2.getDistance(DistanceUnit.CM);
-        distanceS3 = sensorRange3.getDistance(DistanceUnit.CM);
-        //distanceS4 = sensorRange4.getDistance(DistanceUnit.CM);
-        while (opModeIsActive()) {
-            telemetry.addData("Distance1:", distanceS1);
-            telemetry.addData("Distance2:", distanceS2);
-            telemetry.addData("Distance3:", distanceS3);
-            //telemetry.addData("Distance4:", distanceS4);
+        sensorRange4 = hardwareMap.get(DistanceSensor.class, "Ds4");
+
+        // you can also cast this to a Rev2mDistanceSensor if you want to use added
+        // methods associated with the Rev2mDistanceSensor class.
+        Rev2mDistanceSensor sensorTimeOfFlight = (Rev2mDistanceSensor)sensorRange1;
+        Rev2mDistanceSensor sensorTimeOfFlight1 = (Rev2mDistanceSensor)sensorRange1;
+        Rev2mDistanceSensor sensorTimeOfFlight2 = (Rev2mDistanceSensor)sensorRange1;
+        Rev2mDistanceSensor sensorTimeOfFlight3 = (Rev2mDistanceSensor)sensorRange1;
+
+        telemetry.addData(">>", "Press start to continue");
+        telemetry.update();
+
+        waitForStart();
+        while(opModeIsActive()) {
+            // generic DistanceSensor methods.
+
+            telemetry.addData("range", String.format("%.01f cm", sensorRange1.getDistance(DistanceUnit.CM)));
+            telemetry.addData("range", String.format("%.01f cm", sensorRange2.getDistance(DistanceUnit.CM)));
+            telemetry.addData("range", String.format("%.01f cm", sensorRange3.getDistance(DistanceUnit.CM)));
+            telemetry.addData("range", String.format("%.01f cm", sensorRange4.getDistance(DistanceUnit.CM)));
+
+
+            // Rev2mDistanceSensor specific methods.
+            telemetry.addData("ID", String.format("%x", sensorTimeOfFlight.getModelID()));
+            telemetry.addData("ID", String.format("%x", sensorTimeOfFlight1.getModelID()));
+            telemetry.addData("ID", String.format("%x", sensorTimeOfFlight2.getModelID()));
+            telemetry.addData("ID", String.format("%x", sensorTimeOfFlight3.getModelID()));
+            telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight.didTimeoutOccur()));
             telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight1.didTimeoutOccur()));
             telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight2.didTimeoutOccur()));
             telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight3.didTimeoutOccur()));
-           // telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight4.didTimeoutOccur()));
 
-
+            telemetry.update();
         }
-        telemetry.update();
     }
 
-/*
+
+    /*
     public void calcDistance1() {
 
         if (g.getHeading() > 0 && g.getHeading() < 45) {
