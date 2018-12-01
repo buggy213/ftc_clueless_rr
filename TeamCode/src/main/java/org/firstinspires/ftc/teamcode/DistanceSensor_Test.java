f212636530ad362416d828641955934d1e6c8b56
Andrew Zhang <Azhang1818@gmail.com>
2018-11-30 16:17:06 -0800

    Update TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensor_Test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensor_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensor_Test.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -11,11 +12,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.drivetrain_test.FourWheelMecanumDrivetrain;
+import org.firstinspires.ftc.teamcode.drivetrain_test.RobotHardware;
 
 /**
  * Created by DrZ on 11/23/18.
  */
 
+@TeleOp(name="Distance Test")
 public class DistanceSensor_Test extends LinearOpMode {
     private DistanceSensor sensorRange1;
     private DistanceSensor sensorRange2;
@@ -32,10 +36,10 @@ public class DistanceSensor_Test extends LinearOpMode {
     double x;
     double y;
     GyroSensor g;
-    Rev2mDistanceSensor sensorTimeOfFlight1 = (Rev2mDistanceSensor) sensorRange1;
-    Rev2mDistanceSensor sensorTimeOfFlight2 = (Rev2mDistanceSensor) sensorRange2;
-    Rev2mDistanceSensor sensorTimeOfFlight3 = (Rev2mDistanceSensor) sensorRange3;
-    Rev2mDistanceSensor sensorTimeOfFlight4 = (Rev2mDistanceSensor) sensorRange4;
+    //Rev2mDistanceSensor sensorTimeOfFlight1 = (Rev2mDistanceSensor) sensorRange1;
+    //Rev2mDistanceSensor sensorTimeOfFlight2 = (Rev2mDistanceSensor) sensorRange2;
+    //Rev2mDistanceSensor sensorTimeOfFlight3 = (Rev2mDistanceSensor) sensorRange3;
+    //Rev2mDistanceSensor sensorTimeOfFlight4 = (Rev2mDistanceSensor) sensorRange4;
 
 
 
@@ -43,41 +47,71 @@ public class DistanceSensor_Test extends LinearOpMode {
 
     @Override
     public void runOpMode() {
+        RobotHardware rw = new RobotHardware(hardwareMap)
+
+        FourWheelMecanumDrivetrain drivetrain = new FourWheelMecanumDrivetrain(rw);
+
+
+
         // you can use this as a regular DistanceSensor.
         sensorRange1 = hardwareMap.get(DistanceSensor.class, "Ds1");
-        sensorRange2 = hardwareMap.get(DistanceSensor.class, "Ds2");
-        sensorRange3 = hardwareMap.get(DistanceSensor.class, "Ds3");
-        sensorRange4 = hardwareMap.get(DistanceSensor.class, "Ds4");
+        //sensorRange2 = hardwareMap.get(DistanceSensor.class, "Ds2");
+        //sensorRange3 = hardwareMap.get(DistanceSensor.class, "Ds3");
+        //sensorRange4 = hardwareMap.get(DistanceSensor.class, "Ds4");
 
         // you can also cast this to a Rev2mDistanceSensor if you want to use added
         // methods associated with the Rev2mDistanceSensor class.
         Rev2mDistanceSensor sensorTimeOfFlight = (Rev2mDistanceSensor)sensorRange1;
-        Rev2mDistanceSensor sensorTimeOfFlight1 = (Rev2mDistanceSensor)sensorRange1;
-        Rev2mDistanceSensor sensorTimeOfFlight2 = (Rev2mDistanceSensor)sensorRange1;
-        Rev2mDistanceSensor sensorTimeOfFlight3 = (Rev2mDistanceSensor)sensorRange1;
+        //Rev2mDistanceSensor sensorTimeOfFlight1 = (Rev2mDistanceSensor)sensorRange2;
+        //Rev2mDistanceSensor sensorTimeOfFlight2 = (Rev2mDistanceSensor)sensorRange3;
+        //Rev2mDistanceSensor sensorTimeOfFlight3 = (Rev2mDistanceSensor)sensorRange4;
 
         telemetry.addData(">>", "Press start to continue");
         telemetry.update();
 
         waitForStart();
         while(opModeIsActive()) {
+            double turn = (-1) * (gamepad1.left_trigger - gamepad1.right_trigger);
+
+            if (!(gamepad1.left_stick_x == 0 && gamepad1.right_stick_y == 0 && turn == 0)) {
+
+                double speed;
+
+                if (gamepad1.left_stick_x == 0 && gamepad1.right_stick_y == 0) {
+                    speed = 0;
+                }
+                else if ( gamepad1.right_stick_y == 0 ) {
+                    speed = Math.abs(gamepad1.left_stick_x) ;
+                }
+                else if ( gamepad1.left_stick_x == 0 ) {
+                    speed = Math.abs(gamepad1.right_stick_y) ;
+                }
+                else {
+                    speed = ( Math.abs(gamepad1.left_stick_x) + Math.abs(gamepad1.right_stick_y) ) / 2;
+                }
+
+                double angle = Math.atan2(gamepad1.left_stick_x, -gamepad1.right_stick_y);
+                drivetrain.MoveAngle(speed, angle, turn);
+            } else {
+                drivetrain.stop();
+            }
             // generic DistanceSensor methods.
 
             telemetry.addData("range", String.format("%.01f cm", sensorRange1.getDistance(DistanceUnit.CM)));
-            telemetry.addData("range", String.format("%.01f cm", sensorRange2.getDistance(DistanceUnit.CM)));
-            telemetry.addData("range", String.format("%.01f cm", sensorRange3.getDistance(DistanceUnit.CM)));
-            telemetry.addData("range", String.format("%.01f cm", sensorRange4.getDistance(DistanceUnit.CM)));
+            //telemetry.addData("range", String.format("%.01f cm", sensorRange2.getDistance(DistanceUnit.CM)));
+            //telemetry.addData("range", String.format("%.01f cm", sensorRange3.getDistance(DistanceUnit.CM)));
+            //telemetry.addData("range", String.format("%.01f cm", sensorRange4.getDistance(DistanceUnit.CM)));
 
 
             // Rev2mDistanceSensor specific methods.
             telemetry.addData("ID", String.format("%x", sensorTimeOfFlight.getModelID()));
-            telemetry.addData("ID", String.format("%x", sensorTimeOfFlight1.getModelID()));
-            telemetry.addData("ID", String.format("%x", sensorTimeOfFlight2.getModelID()));
-            telemetry.addData("ID", String.format("%x", sensorTimeOfFlight3.getModelID()));
+            //telemetry.addData("ID", String.format("%x", sensorTimeOfFlight1.getModelID()));
+            //telemetry.addData("ID", String.format("%x", sensorTimeOfFlight2.getModelID()));
+            //telemetry.addData("ID", String.format("%x", sensorTimeOfFlight3.getModelID()));
             telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight.didTimeoutOccur()));
-            telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight1.didTimeoutOccur()));
-            telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight2.didTimeoutOccur()));
-            telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight3.didTimeoutOccur()));
+            //telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight1.didTimeoutOccur()));
+            //telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight2.didTimeoutOccur()));
+            //telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight3.didTimeoutOccur()));
 
             telemetry.update();
         }
